9b585561b32cec3e36adeed6e9c2b3eaffc14940
Luan Gerber Siiss <luangerber666@gmail.com>
2024-07-30 16:01:50 -0300

    tentandoDeOutraForma

--- HackerRank/algoritmo/intermediario/Forming_a_Magic_Square.js
+++ HackerRank/algoritmo/intermediario/Forming_a_Magic_Square.js
@@ -1,78 +1,18 @@
 function formingMagicSquare(s) {
     // Write your code here
     // O numero magico sera definido como a maior some entre as linhas e colunas
-    let mudancasTotais = 0
-
-    function repetiu(arr, num, l, c) {
-        let cont = 0
-        
-        for (let i = 0; i < arr.length; i++) {
-            for (let j = 0; j < arr[i].length; j++) {
-                if (num === arr[i][j]) {
-                    cont += 1
-                    if (cont > 1) {
-                        l = i
-                        c = j
-                    }
-                        
-                } 
-            }
-        }
-
-        return cont > 1 ? [true, l, c] : [false, l, c]
-    }
-
-    //ANTES
-    console.log(s)
-
-    for (let i = 0; i < s.length; i++) {
-        for (let j = 0; j < s[i].length; j++) {
-            let somaColuna = 0
-            let somaLinha = 0
-            let MatrizMagica = false
-            while (MatrizMagica == false) {
-                somaColuna = 0
-                somaLinha = 0
-                for (let c = 0; c < s[j].length; c++) {
-                    somaLinha += s[i][c]
-                    somaColuna += s[c][j] 
-                    
-                }
-                if (somaColuna != 15 && somaLinha != 15 && repetiu(s, s[i][j])[0] == false) {
-                    console.log('ta aqui')
-                    console.log(s[i][j])
-                    s[i][j] += 1
-                    mudancasTotais += 1
-                } else if (somaColuna != 15 && somaLinha != 15 && repetiu(s, s[i][j])[0]) {
-                    let linha = repetiu(s, s[i][j])[1]
-                    let coluna = repetiu(s, s[i][j])[2]
-                    let somaColunades = 0
-                    let somaLinhades = 0
-                    for (let c = 0; c < 3; c++) {
-                        somaLinhades += s[linha][c]
-                        somaColunades += s[c][coluna]
-                    }
-                    if (somaColunades != 15 && somaLinhades != 15) {
-                        s[linha][coluna] += 1
-                        mudancasTotais += 1
-                    } else {
-                        s[i][j] += 1
-                        mudancasTotais += 1
-                    }
-                } else {
-                    console.log('opa')
-                    MatrizMagica = true
-                }
-            }
-        }
-    }        
-
-    //DEPOIS
-    console.log(s)
-    console.log(mudancasTotais)
-
-    return mudancasTotais
-    
+    const numMagico = s.length * s.length
+    const matrizesPossiveis = [
+        [[8, 1, 6], [3, 5, 7], [4, 9, 2]],
+        [[4, 3, 8], [9, 5, 1], [2, 7, 6]],
+        [[2, 9, 4], [7, 5, 3], [6, 1, 8]],
+        [[6, 7, 2], [1, 5, 9], [8, 3, 4]],
+        [[6, 1, 8], [7, 5, 3], [2, 9, 4]],
+        [[4, 9, 2], [3, 5, 7], [8, 1, 6]],
+        [[8, 3, 4], [1, 5, 9], [6, 7, 2]],
+        [[2, 7, 6], [9, 5, 1], [4, 3, 8]],
+    ]
+    //continuacao
 }
 
 console.log(formingMagicSquare([[4, 9, 2], [3, 5, 7], [8, 1, 5]]))
--- HackerRank/algoritmo/intermediario/umSonho.js
+++ HackerRank/algoritmo/intermediario/umSonho.js
@@ -1,5 +1,7 @@
 function formingMagicSquare(s) {
     // Write your code here
+    let numeroMagico = 15
+    let tamanhoMatriz = 9
     let mudancasTotais = 0
 
     for (let i = 0; i < s.length; i++) {
@@ -19,18 +21,34 @@ function formingMagicSquare(s) {
                     diagonalSecundaria += s[c][s.length - c - 1] //percorre a diagonal secundaria apenas com 1 for
                 }
 
-                if ((somaColuna < 15 && somaLinha < 15) && somaColuna === somaLinha) {
-                    if ((i == j && diagonalPrincipal == 15) || (i + j == s.length - 1 && diagonalSecundaria == 15)) { // caso esteja na diagonal principal ou secundaria o tratamento é diferente
+                if ((somaColuna < 15 && somaLinha < 15) && somaColuna === somaLinha && s[i][j] < tamanhoMatriz) {
+                    if ((i == j && diagonalPrincipal == 15) || (i + j == s.length - 1 && diagonalSecundaria == 15)) { // caso esteja na diagonal principal ou secundaria o tratamento e diferente
                         MatrizMagica = true
                     } else {
                         s[i][j] += 1
                         mudancasTotais += 1
                     }
-                } else if (somaColuna > 15 || somaLinha > 15) {
+                } else if ((somaColuna > 15 && somaLinha > 15)) {
+                    if ((i == j && diagonalPrincipal == 15) || (i + j == s.length - 1 && diagonalSecundaria == 15)) { // caso esteja na diagonal principal ou secundaria o tratamento e diferente
+                        MatrizMagica = true
+                    } else {
+                        s[i][j] -= 1
+                        mudancasTotais += 1
+                    }
+                } else if ((somaColuna > 15 || somaLinha > 15) && (diagonalPrincipal < 15 || diagonalSecundaria <  15) && ((i != j) && (i + j != s.length - 1))) {
                     s[i][j] -= 1
                     mudancasTotais += 1
-                }
-                else {
+                } else if ((i == j) || (i + j == s.length - 1)) {
+                    if (i == j && diagonalPrincipal < 15 && (somaColuna < 15 || somaLinha < 15)) {
+                        s[i][j] += 1
+                        mudancasTotais += 1
+                    } else if (i + j == s.length - 1 && diagonalSecundaria < 15 && (somaColuna < 15 || somaLinha < 15)) {
+                        s[i][j] += 1
+                        mudancasTotais += 1
+                    } else {
+                        MatrizMagica = true
+                    }
+                } else {
                     MatrizMagica = true
                 }
             }
@@ -45,4 +63,5 @@ function formingMagicSquare(s) {
 console.log(formingMagicSquare([[4, 9, 2], [3, 5, 7], [8, 1, 5]]))
 console.log(formingMagicSquare([[5, 3, 4], [1, 5, 8], [6, 4, 2]]))
 console.log(formingMagicSquare([[4, 8, 2], [4, 5, 7], [6, 1, 6]]))
-console.log(formingMagicSquare([[4, 5, 8], [2, 9, 1], [1, 9, 7]]))
+console.log(formingMagicSquare([[4, 5, 8], [2, 4, 1], [1, 9, 7]]))
+console.log(formingMagicSquare([[2, 9, 8], [4, 2, 7], [5, 6, 7]]))
